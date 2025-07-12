2d0c785aef1993791711a304107b9aeece25c176
lucas19961 <lucasbergmann@t-online.de>
2025-07-12 16:36:58 +0200

    chore: added github issues to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,19 +4,28 @@ import { Link } from '@/components/ui/link';
 
 export default function Footer() {
 	return (
-		<footer className="flex flex-col justify-center gap-1 p-2 text-center text-font-dark md:flex-row md:gap-2">
-			<p>
-				&copy; {new Date().getFullYear()} <Link href="/">susgee.dev</Link>
-			</p>
-			<span className="hidden md:flex"> • </span>
-			<p>not affiliated with twitch</p>
-			<span className="hidden md:flex"> • </span>
-			<p>
-				Made with 🤍 by
-				<Link className="pl-1" href="https://twitch.tv/maersux" rel="noreferrer" target="_blank">
-					maersux
+		<footer>
+			<p className="text-center">
+				Ideas for improvements? Discovered a bug? Feel free to create an{' '}
+				<Link href="https://github.com/susgee-dev/susgee-history/issues/new" target="_blank">
+					issue on our GitHub
 				</Link>
+				.
 			</p>
+			<div className="flex flex-col justify-center gap-1 p-2 text-center text-font-dark md:flex-row md:gap-2">
+				<p>
+					&copy; {new Date().getFullYear()} <Link href="/">susgee.dev</Link>
+				</p>
+				<span className="hidden md:flex"> • </span>
+				<p>not affiliated with twitch</p>
+				<span className="hidden md:flex"> • </span>
+				<p>
+					Made with 🤍 by
+					<Link className="pl-1" href="https://twitch.tv/maersux" rel="noreferrer" target="_blank">
+						maersux
+					</Link>
+				</p>
+			</div>
 		</footer>
 	);
 }

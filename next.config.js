7fc7f042625421ee6de07bce7056b567942f82db
Marcel <marcel.gertsch@update.ch>
2025-07-29 13:56:21 +0200

    fix: added ffz/bbtv remove patterns to config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{ protocol: 'https', hostname: 'static-cdn.jtvnw.net' },
-			{ protocol: 'https', hostname: 'cdn.7tv.app' }
+			{ protocol: 'https', hostname: 'cdn.7tv.app' },
+			{ protocol: 'https', hostname: 'cdn.frankerfacez.com' },
+			{ protocol: 'https', hostname: 'cdn.betterttv.net' }
 		]
 	}
 };
